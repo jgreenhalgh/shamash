40182e4db51b1b8439e7688692ed5db658ef9def
root <root@shamashhost.members.linode.com>
2012-11-08 16:30:20 -0500

    github.php change

--- github.php
+++ github.php
@@ -1,1 +1,10 @@
-<?php `git pull`;
+ <?php 
+ $File = "YourFile.txt"; 
+ $Handle = fopen($File, 'w');
+ $Data = "Jane Doe\n"; 
+ fwrite($Handle, $Data); 
+ $Data = "Bilbo Jones\n"; 
+ fwrite($Handle, $Data); 
+ print "Data Written"; 
+ fclose($Handle); 
+ ?>
